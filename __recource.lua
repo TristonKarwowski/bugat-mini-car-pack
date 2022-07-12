9929c6ebc1e20b96dbf625f6ccf4f7ec696e68fa
TristonKarwowski <109119648+TristonKarwowski@users.noreply.github.com>
2022-07-11 23:03:27 -0400

    Add files via upload

---
+++ __recource.lua
@@ -0,0 +1,14 @@
+author 'YOURNAME From Hunters Development'
+description ''
+repository 'https://discord.gg/ZhNtejyzEy'
+
+files {
+    'data/**/*.meta',
+}
+
+
+data_file 'HANDLING_FILE'            'data/**/handling*.meta'
+data_file 'VEHICLE_LAYOUTS_FILE'    'data/**/vehiclelayouts*.meta'
+data_file 'VEHICLE_METADATA_FILE'    'data/**/vehicles*.meta'
+data_file 'CARCOLS_FILE'            'data/**/carcols*.meta'
+data_file 'VEHICLE_VARIATION_FILE'    'data/**/carvariations*.meta'
